318ea450e516cb634b21584df15134528b8448ef
Chris De Lima <delimac@hey.com>
2021-08-03 23:16:21 +0800

    added lineheight range slider

--- js/typechecker.js
+++ js/typechecker.js
@@ -1,12 +1,18 @@
 const sentenceTag = document.querySelector(`input[type="text"]`)
+
 const typesizeTag = document.querySelector(`input[name="typesize"]`)
 const typesizeOutput = document.querySelector("span.typesize-output")
+
+const lineheightTag = document.querySelector(`input[name="lineheight"]`)
+const lineheightOutput = document.querySelector("span.lineheight-output")
+
 const outputTag = document.querySelector("textarea.output")
 const originalText = outputTag.value
 
 // when user types in sentenceTag, update outputTag accordingly
 // if there is no value, restore original text
 sentenceTag.addEventListener("keyup", function () {
+  // "this" refers to sentenceTag
   if (this.value) {
     outputTag.innerHTML = this.value
   } else {
@@ -22,6 +28,13 @@ outputTag.addEventListener("keyup", function () {
 // when user adjusts type size slider, update the text indicator next to it and 
 // change the outputTag's font size
 typesizeTag.addEventListener("input", function () {
+  // "this" here refers to typesizeTag
   outputTag.style.fontSize = this.value + "px"
   typesizeOutput.innerHTML = this.value + "px"
+})
+
+lineheightTag.addEventListener("input", function () {
+  // "this" here refers to lineheightTag
+  outputTag.style.lineHeight = this.value
+  lineheightOutput.innerHTML = this.value
 })
